5f2c8fff727a60a24054163d045bbf275b0b7267
EdeiltonSO <edeiltonso@gmail.com>
2020-02-14 05:26:13 -0300

    style changes

--- src/App.js
+++ src/App.js
@@ -28,32 +28,43 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="content">
+        <p id="title">
+          base<strong>converter</strong>app
+        </p>
+
         <form onSubmit={this.returnApi}>
+          <p className="subtitle">BASES</p>
           <input
+            className="base"
             type="text"
-            placeholder="Base de entrada"
+            placeholder="entrada"
             value={this.state.baseIn}
             onChange={e => this.setState({ baseIn: e.target.value })}
           />
-          <br />
+          <span id="to">»</span>
           <input
+            className="base"
             type="text"
-            placeholder="Base de saída"
+            placeholder="saída"
             value={this.state.baseOut}
             onChange={e => this.setState({ baseOut: e.target.value })}
           />
           <br />
+          <p className="subtitle">VALORES</p>
           <input
+            className="valueIn"
             type="text"
-            placeholder="Valor de entrada"
+            placeholder="entrada"
             value={this.state.valueIn}
             onChange={e => this.setState({ valueIn: e.target.value })}
           />
           <br />
-          <button type="submit">Converter</button>
+          <h1 className="valueOut">{this.state.valueOut}</h1>
+          <button className="convert" type="submit">
+            CONVERTER
+          </button>
         </form>
-        <h1>{this.state.valueOut}</h1>
       </div>
     );
   }
