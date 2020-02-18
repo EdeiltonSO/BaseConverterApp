c0c50376619818a23126c13bc7373187a91e95f4
EdeiltonSO <edeiltonso@gmail.com>
2020-02-18 08:36:08 -0300

    avoid app fall asleep

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+var http = require("http");
+setInterval(function() {
+  http.get("http://baseconverterapp.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
+
 ReactDOM.render(<App />, document.getElementById("root"));
